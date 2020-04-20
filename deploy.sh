ea789f41ef321ad08e9d717efd7a7cb76186fd5a
iYaroslav <samardak.yaroslav@gmail.com>
2020-04-20 09:44:15 +0500

    Changed repository scope

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 if [[ -z "$(git status --untracked-files=no --porcelain)" ]]; then
   version=`yarn version --patch | awk '/New version:/ {print $4}'`
   echo "Version incremented to ${version}"
-  yarn publish --new-version ${version}
+  yarn publish --new-version ${version} --access public
+  npm publish --access public
   gh-pages -d example/build
   silent=`git push origin`
   silent=`git push origin --tags`
